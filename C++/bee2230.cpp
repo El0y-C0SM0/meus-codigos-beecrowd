2b5cd7eb588b25b2340df34e10f9f407f67ff566
El0y-C0SM0 <eloymaciel06@gmail.com>
2024-04-11 11:11:07 -0300

    bee2230 em processo

--- C++/bee2230.cpp
+++ C++/bee2230.cpp
@@ -1,3 +1,4 @@
+// ! Runtime error
 #include <bits/stdc++.h>
 
 #define endl '\n'
@@ -5,6 +6,7 @@
 #define dbg(a) (cout << '\n' << #a << " = " << a << '\n')
 #define loop(a,m) for(int i = a; i < m; ++i)
 #define pb push_back
+#define ins insert
 #define f first
 #define s second
 
@@ -14,11 +16,72 @@ typedef uint32_t uint;
 typedef long long ll;
 typedef long double ld;
 typedef vector<int> vtr;
+typedef vector<vtr> gph;
 
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3f;
 
+gph cid;
+set<int> vis;
+queue<pair<int, int>> q;
+int p, l;
+
+void bfs() {
+
+    if(!p) return;
+
+    for(int i : cid[l]) {
+        q.push({i, 1});
+        vis.ins(i);
+    }
+
+    while (!q.empty()) {
+        auto x = q.front();  q.pop();
+        if(x.s >= p) return;
+
+        for(int i : cid[x.f]) if(!vis.count(i)) {
+            q.push({i, x.s + 1});
+            vis.ins(i);
+        }
+    }
+    
+}
+
+void zerarQueue() {
+    while (!q.empty()) q.pop();
+    
+}
+
 int main() {_
+    int cont{1};
+
+    while(1) {
+        int c, e;
+        cin >> c >> e >> l >> p;
+        l--;
+        
+        cid = gph(c);
+
+        loop(0, e) {
+            int x, y;
+            cin >> x >> y; x--; y--;
+
+            cid[x].pb(y);
+            cid[y].pb(x);
+        }
+
+        bfs();
+        if(vis.count(l)) vis.erase(l);
+
+        cout << "Teste " << cont++ << endl;
+        for(int i : vis) {
+            cout << i + 1 << ' ';
+        }
+        cout << endl << endl;
+        
+        vis.clear();
+        zerarQueue();
+    }
 
     return 0;
 }
