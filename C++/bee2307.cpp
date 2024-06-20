1aaed51b5657957665a0ee1d1058ab9e5b5219d8
El0y-C0SM0 <eloymaciel06@gmail.com>
2024-06-20 00:06:43 -0300

    bee2307.cpp accepted

---
+++ C++/bee2307.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(NULL);
+#define endl '\n'
+#define dbg(a) (cout << #a << " = " << a << '\n')
+#define loop(i,a,m) for(int i = a; i < m; ++i)
+#define pb push_back
+#define f first
+#define s second
+
+using namespace std;
+
+typedef uint32_t uint;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vtr;
+typedef pair<int,int> par;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main() {_
+    int n; cin >> n;
+    int rod{n};
+    unordered_set<int> morto;
+
+    loop(i, 0, n) {
+        int x; cin >> x;
+        morto.insert(x);
+        if(morto.count(x - 1)) rod--;
+    }
+
+    cout << rod << endl;
+
+    return 0;
+}
