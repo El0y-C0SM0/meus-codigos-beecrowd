bc36de6800dfeeb237ca0bb7682039dc850b1fda
El0y-C0SM0 <eloymaciel06@gmail.com>
2024-06-22 21:35:42 -0300

    bee1126.cpp accepted

---
+++ C++/bee1126.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(NULL);
+#define endl '\n'
+#define dbg(a) (cout << #a << " = " << a << '\n')
+#define loop(i,a,m) for(int i = a; i < m; ++i)
+#define pb push_back
+#define f first
+#define s second
+
+using namespace std;
+
+typedef uint32_t uint;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vtr;
+typedef pair<int,int> par;
+typedef tuple<int,int,int> trio;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+struct Nave {
+    trio p1, p2, p3, p4;
+
+    Nave(trio p1, trio p2, trio p3, trio p4) : p1(p1), p2(p2), p3(p3), p4(p4) {}
+};
+
+class Solution {
+    Solution(trio p1, trio p2, trio p3, trio p4) {
+
+    }
+};
+
+int main() {_
+    int casos; cin >> casos;
+
+    cout << fixed << setprecision(2);
+    trio pontos[8];
+
+
+    return 0;
+}
